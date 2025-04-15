d9f76eba13aefb48630d41b0a8e066eb4006494a
Maksim Marinov <maks1412@gmail.com>
2025-04-15 12:51:28 +0300

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 import { Space_Mono, Space_Grotesk } from "next/font/google";
 import "./globals.css";
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={` ${spaceGrotesk.variable} ${spaceMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
